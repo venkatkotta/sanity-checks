2d065d75dc4589de091d35b2f3ee754c45c88e44
Venkat <venkat.kotta@accesscfa.com>
2021-06-28 12:25:43 -0400

    working script

--- instance.py
+++ instance.py
@@ -1,104 +1,92 @@
-#import __init__
-
-#from AWS.main import service
 import boto3
 import urllib3
 from prettytable import PrettyTable
-#from botocore.exceptions import ClientError
-
+import sys
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-#zcs_ec2 = service.AwsService('ec2')
-
-#client = zcs_ec2.get_aws_client_sessions()
-#resource = zcs_ec2.get_aws_resource_sessions()
-
-region = 'us-west-2'
-client = boto3.session.Session().client('ec2', region_name = region)
+regions = ['us-west-2']
 
 result = PrettyTable(['Region', 'InstanceType', 'Instances', 'InstanceCPU', 'TotalCPU'])
 totalaz_sum = PrettyTable(['Region', 'AZ', 'Instances'])
 totalcpu_sum = PrettyTable(['Region', 'SumTotalCPU', 'Instances'])
 
 
-ec2_spotcap = []
+ec2_cap = []
 ec2_azcap = []
 
 totalcpu_counter = 0
 totalinstance_counter = 0
 
-#import pdb
-#pdb.set_trace()
+for region in regions:
 
-response = client.describe_instances()
+    client = boto3.session.Session().client('ec2', region_name = region)
+    response = client.describe_instances()
 
-if len(response['Reservations']) > 0:
+    if len(response['Reservations']) > 0:
 
         for reservation in response['Reservations']:
-                for instance in reservation['Instances']:
 
-                        try:
+            for instance in reservation['Instances']:
 
-                                if instance['State']['Name'] == 'running': # and instance['InstanceLifecycle'] == 'spot':
+                try:
 
-                                        ec2_occurances = False
-                                        az_occurances = False
+                    if instance['State']['Name'] == 'running':
 
-                                        vcpu = client.describe_instance_types(InstanceTypes=[instance['InstanceType']])['InstanceTypes'][0]['VCpuInfo']['DefaultVCpus']
-                                        #vcpu = instance['CpuOptions']['CoreCount'] * instance['CpuOptions']['ThreadsPerCore']
+                        ec2_occurances = False
+                        az_occurances = False
 
-                                        for spot in ec2_spotcap:
+                        vcpu = client.describe_instance_types(InstanceTypes=[instance['InstanceType']])['InstanceTypes'][0]['VCpuInfo']['DefaultVCpus']
+                        #vcpu = instance['CpuOptions']['CoreCount'] * instance['CpuOptions']['ThreadsPerCore']
 
-                                                if spot['InstanceType'] == instance['InstanceType']:
+                        for compute in ec2_cap:
 
-                                                        spot['Instances'] += 1
-                                                        spot['TotalCPU'] += vcpu
+                            if compute['InstanceType'] == instance['InstanceType']:
 
-                                                        break
+                                compute['Instances'] += 1
+                                compute['TotalCPU'] += vcpu
 
-                                if not ec2_occurances:
+                                break
 
-                                        ec2_spotcap.append({'Region': region, 'Instancetype': instance['InstanceType'],
-                                    'Instances': 1, 'TotalCPU': vcpu})
+                    if not ec2_occurances:
 
-                                for az in ec2_azcap:
+                        ec2_cap.append({'Region': region, 'InstanceType': instance['InstanceType'], 'Instances': 1, 'TotalCPU': vcpu})
 
-                                        if az['AZ'] == instance['Placement']['AvailabilityZone']:
+                    for az in ec2_azcap:
 
-                                                az['Region'] = region
-                                                az['Instances'] += 1
+                        if az['AZ'] == instance['Placement']['AvailabilityZone']:
 
-                                                az_occurances =  True
+                            az['Region'] = region
+                            az['Instances'] += 1
 
-                                                break
+                            az_occurances =  True
 
-                                if not az_occurances:
+                            break
 
-                                        ec2_azcap.append({'Region': region, 'AZ': instance['Placement']['AvailabilityZone'],
-                                  'Instances': 1})
+                    if not az_occurances:
 
-                        except:
+                        ec2_azcap.append({'Region': region, 'AZ': instance['Placement']['AvailabilityZone'], 'Instances': 1})
 
-                                pass
+                except Exception as e:
+                    print("Exiting the Script.Something is not right, please check the Exception below")
+                    print(e)
+                    sys.exit()
+for compute in ec2_cap:
 
-for spot in ec2_spotcap:
+    result.add_row([region, compute['InstanceType'], compute['Instances'], int(compute['TotalCPU']/compute['Instances']), compute['TotalCPU']])
 
-        result.add_row([region, spot['InstanceType'], spot['Instances'], int(spot['TotalCPU']/spot['Instances']), spot['TotalCPU']])
-
-        totalcpu_counter += spot['TotalCPU']
-        totalinstance_counter += spot['Instances']
+    totalcpu_counter += compute['TotalCPU']
+    totalinstance_counter += compute['Instances']
 
 if totalcpu_counter > 0:
 
-        totalcpu_sum.add_row([region, totalcpu_counter, totalinstance_counter])
+    totalcpu_sum.add_row([region, totalcpu_counter, totalinstance_counter])
 
 for az in ec2_azcap:
 
-        totalaz_sum.add_row([region, az['AZ'], az['Instances']])
+    totalaz_sum.add_row([region, az['AZ'], az['Instances']])
 
-
-result.sortby = "TotalCPU"
+result.sortby = "InstanceType"
 totalaz_sum.sortby = "Instances"
 totalcpu_sum.sortby = "Region"
 
